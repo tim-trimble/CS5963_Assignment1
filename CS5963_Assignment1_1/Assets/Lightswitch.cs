eaec51d63ae2b12486db50d5b00061e11a6402e7
greyvest <greyvestanderson@gmail.com>
2019-09-01 14:03:21 -0600

    Built scene, added scripts

---
+++ CS5963_Assignment1_1/Assets/Lightswitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+
+    List<Color> coloArray = new List<Color>{ Color.blue, Color.red, Color.green, Color.cyan, Color.yellow };
+
+    public Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+    
+    int i = 0;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown("tab"))
+        {
+            light.color = coloArray[i];
+            i++;
+            if (i > coloArray.Count-1)
+                i = 0;
+        }
+    }
+}
